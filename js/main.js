00db559c40d408bc2b963df55e241754df9b2706
willcodes <difruscio.dev@gmail.com>
2016-09-13 13:26:14 -0400

    added images styling

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ var attempsRemaining=10;
 var gameState="";
 var output = document.getElementById("output");
 var gameWon = false;
+var score = document.getElementById("scoreboard");
+guessClear = document.getElementById("guess");
+console.log(answer);
 
 window.addEventListener("keydown", keydownHandler, false); 
 function keydownHandler(event)
@@ -15,11 +18,9 @@ function keydownHandler(event)
  validate();
  }
 }
-
 submit.onclick = function(){
 	validate();
 }
-
 function validate(){
 	guess = document.getElementById("guess").value;  // mystery number value;
 	if (isNaN(guess) || guess == ""){
@@ -33,23 +34,35 @@ function playGame(){
 	attempsRemaining --;
 	attemptsMade ++;
 	gameState = "Attempts: " + attemptsMade + "<br>" + "Remaining: " + attempsRemaining;
-	if (guess == answer) {
+	if (guess > 100) {
+		scoreboard.innerHTML = gameState;
+		output.innerHTML = "Pick a smaller number -_-";
+	}
+	else if (guess == answer) {
 		gameWon = true;
-		output.innerHTML = "Correct! "+ "<br>" + gameState;
+		output.style.color = "forestgreen";
+		output.innerHTML = "Correct! ";
+		scoreboard.innerHTML = gameState;
 		endGame();
 	}
 	else if (guess > answer){
-		output.innerHTML = "Incorrect, Too high :( "+"<br>" + gameState;
+		output.style.color = "red";
+		output.innerHTML = "Incorrect, Too high :(";
+		scoreboard.innerHTML = gameState;
 		if (attempsRemaining < 1){
 			endGame();
 		}
 	}
 	else if(guess < answer){
+		output.style.color = "red";
 		output.innerHTML = "Incorrect, Toooo low :O " + "<br>" + gameState;
 		if (attempsRemaining < 1){
 			endGame();
 		}
 	}
+	render();
+	guessClear.value = "";
+	
 };
 function endGame(){
  	if (gameWon){
@@ -65,6 +78,16 @@ function endGame(){
  	submit.disabled = true;
 	window.removeEventListener("keydown", keydownHandler, false);
  	guess.disabled = true;
-};	 
+};
+//The arrow
+var arrow = document.querySelector("#arrow"); 
+function render(){
+	if (guess > 100) {
+	}
+	else{
+		arrow.style.left = guess * 3 + "px";
+	}
+
+}	 
 
 
